f3efd7a22e476c0093b0fccc6baa308fb4d8c6fe
Ashutosh <ashutoshb2000@gmail.com>
2021-06-10 20:55:03 +0100

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Landingpage from "./Landingpage";
+import About from "./About"
 import Menu from "./Menu";
 
 import {Switch,Route, Redirect} from "react-router";
@@ -9,8 +10,8 @@ const App = () =>{
         <Menu/>
         <Switch>
             <Route exact path="/" component={Landingpage} />
-            {/* <Route exact path="/about" component={About}/>
-            <Route exact path="/skills" component={Skills}/>
+            <Route exact path="/about" component={About}/>
+            {/* <Route exact path="/skills" component={Skills}/>
             <Route exact path="/projects" component={Projects}/>
             <Route exact path="/education" component={Education}/>
             <Route exact path="/experience" component={Experience}/>
